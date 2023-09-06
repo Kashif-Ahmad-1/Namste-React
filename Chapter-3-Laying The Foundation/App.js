f7baa6468e14cb19def9441cb0f0dfde57c7d602
Kashif Ahmad <19bcs2983@gmail.com>
2023-09-06 21:07:45 +0530

    JSX

--- Chapter-3-Laying The Foundation/App.js
+++ Chapter-3-Laying The Foundation/App.js
@@ -3,8 +3,15 @@ import ReactDOM from "react-dom/client";
 
 // React.createElement ==> Object ==> HTMLElement(render)
 
-const heading = React.createElement("h1",{id: "heading"},"Namaste React");
+const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
+
+// JSX --> is not HTML in JS
+// JSX ==> React.createElement ==> Object ==> HTMLElement(render)
+const jsxHeading = <h1 id="heading">Namaste React using JSX</h1>;
+
+// {both heading and jsxHeading are same only written in Different way}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+// root.render(heading);
+root.render(jsxHeading);

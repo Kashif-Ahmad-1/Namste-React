55b7cd1fa70fce19b99e19897a32b3a86e355315
Kashif Ahmad <19bcs2983@gmail.com>
2023-09-06 22:02:58 +0530

    Function component

--- Chapter-3-Laying The Foundation/App.js
+++ Chapter-3-Laying The Foundation/App.js
@@ -6,12 +6,55 @@ import ReactDOM from "react-dom/client";
 const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
 
 // JSX --> is not HTML in JS
-// JSX ==> React.createElement ==> Object ==> HTMLElement(render)
-const jsxHeading = <h1 id="heading">Namaste React using JSX</h1>;
+// JSX ==>Babel transpiles to React.createElement ==> Object ==> HTMLElement(render)
+
+// if code contains only one line then dont wrap with this bracket
+
+
+// const jsxHeading = (
+//   <h1 className="head" tabIndex="5">
+//     Namaste React using JSX
+//   </h1>
+// ); 
+
+
+// Manadatory to use () for multiple line
 
 // {both heading and jsxHeading are same only written in Different way}
+
+// React Component
+// Class Based Component - OLD
+// Functional Component - New
+
+const Title = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React using JSX
+  </h1>
+); 
+
+
+// React Functional Component
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      <Title />      
+       {/* all code of title comes here */}
+      <h1 className="head" tabIndex="5">
+        Namaste React using Functional Component
+      </h1>
+    </div>
+  );
+};
+
+// Another Syntax
+const HeadingComponent2 = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React using Functional Component 2
+  </h1>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // root.render(heading);
-root.render(jsxHeading);
+// root.render(jsxHeading);
+root.render(<HeadingComponent />);

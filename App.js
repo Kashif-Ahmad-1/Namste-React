68e71094c88b5bbd93552c4ce121fa8827598fbb
Kashif Ahmad <19bcs2983@gmail.com>
2023-09-16 10:45:58 +0530

    final file

--- App.js
+++ App.js
@@ -43,7 +43,7 @@ const RestaurantCard = (props) => {
           "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
           cloudinaryImageId
         }
-      />
+      /> 
       <h3>{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRating} stars</h4>

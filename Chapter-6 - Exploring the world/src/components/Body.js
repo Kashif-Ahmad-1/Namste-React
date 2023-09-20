dd0e1fd75561613382836fff1f487ffc5c548127
Kashif Ahmad <19bcs2983@gmail.com>
2023-09-20 13:03:31 +0530

    Swiggy Api issue

---
+++ Chapter-6 - Exploring the world/src/components/Body.js
@@ -0,0 +1,47 @@
+import RestaurantCard from "./RestaurantCard";
+import { useState, useEffect } from "react";
+import resList from "../utils/mockData";
+
+const Body = () => {
+
+
+const [ListofRes, setListofRes] = useState(resList)
+useEffect(()=>{
+  
+  fetchData();
+}, []);
+
+const fetchData = async () => {
+  const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&collection=83667');
+  const json = await data.json();
+  console.log("apiData", json);
+ 
+  setListofRes(json.data.cards[3]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+}
+console.log("ResList", resList);
+
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            
+
+           const filteredList = ListofRes.filter((res)=> res.data.avgRating > 4);
+           setListofRes(filteredList)
+          }}
+        >
+          Top rated Restaurant
+        </button>
+      </div>
+      <div className="res-container">
+        {ListofRes.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
